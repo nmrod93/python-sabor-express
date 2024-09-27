e92bc7b6eb87abee17fb0516e7311b210b5d1a52
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-09-27 20:09:54 -0300

    feat:constructor + str

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,15 +1,20 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return self.nome
+        return f'{self.nome} | {self.categoria}'
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} {restaurante.ativo}')
 
 restaurante_praca = Restaurante('Praca', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-print(restaurante_praca)
-
-'teste'
+Restaurante.listar_restaurantes()
