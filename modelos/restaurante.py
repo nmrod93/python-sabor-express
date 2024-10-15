d7ba1b5fef942748bedb386203b52212e9b71cff
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-10-15 10:18:04 -0300

    feat: changing status

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,16 +10,21 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
 
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return 'ativo' if self._ativo else 'desativo'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praca', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
