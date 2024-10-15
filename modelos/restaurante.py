ba35d05cafeff63d4724f8b0a9cd48749f6d41aa
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-10-15 10:08:49 -0300

    feat:private nome

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,13 +2,13 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
 
     def listar_restaurantes():
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
