923e2e33277d09c2b84e23011746f7c9ad0da40b
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-10-30 00:35:15 -0300

    feat: media_avaliacoes to str

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Nicolas', 10)
+restaurante_praca.receber_avaliacao('Nicolas', 2)
 
 def main():
    Restaurante.listar_restaurantes() 
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -15,9 +15,9 @@ class Restaurante:
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -32,7 +32,7 @@ class Restaurante:
 
     @property
     def media_avaliacoes(self):
-        if not self.avaliacao:
+        if not self._avaliacao:
             return 0
         
         soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
