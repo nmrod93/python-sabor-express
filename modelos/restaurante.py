94741cdd623b8f0d036981485b9b2ed40dff7cd7
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-10-30 00:27:42 -0300

    feat: creating avaliacoes property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -29,3 +29,13 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self.avaliacao:
+            return 0
+        
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_notas = len(self._avaliacao)
+        media = round(soma_notas / quantidade_notas, 1)
+        return media
